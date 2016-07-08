166247f212c492525796d74ff4c8c24f1c6c80a6
Alex Riviere <fimion@gmail.com>
2016-07-08 01:08:40 -0400

    added templates
    made some basic template organization.

--- podium/views.py
+++ podium/views.py
@@ -1,8 +1,8 @@
 # Put all Flask views in here, as suggested by:
 # http://flask.pocoo.org/docs/0.11/patterns/packages/
 from podium import app
-
+from flask import render_template
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    return render_template("index.html")
